4a8d31200121c86b4a935859d3f0e291292359ed
safaribrowser69-eng <safaribrowser69@gmail.com>
2026-07-29 17:29:43 +0530

    Update and rename next.config.ts to next.config.js

--- care-union/next.config.js
+++ care-union/next.config.js
@@ -1,5 +1,4 @@
-import type { NextConfig } from 'next'
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {protocol:'https',hostname:'images.unsplash.com'},
